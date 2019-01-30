c755c6264fcae36688a092978823cea8471e852b
github username <github email address>
2019-01-30 00:28:04 -0500

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,24 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+    console.log("The DOM content has loaded!")
+
+    const form = document.querySelector('#create-task-form')
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const ul = document.getElementById('list')
+        const todoitem = document.getElementById('new-task-description')
+
+        let taskitem = document.createElement('li')
+        taskitem.setAttribute('class', 'task-items')
+
+        taskitem.textContent = todoitem.value
+        ul.appendChild(taskitem)
+
+        event.target.reset()
+    };
+
+    form.addEventListener('submit', handleSubmit)
+
 });
