69307058959152a40ac064a44832663a28a04362
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-01-30 08:37:20 -0500

    added close button

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,16 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
   const taskContainer = document.getElementById('list')
-  const taskForm = document.getElementById('create-task-form')
+  const taskForm = document.getElementById('create-task-form') 
 
   taskForm.addEventListener('submit', function(event) {
     event.preventDefault() //stop form from POSTING
     const userInputField = event.target.querySelector('#new-task-description') 
 
-    //OR document.querySelector('#new-comment')
     const userInputString = userInputField.value
     const newTask = document.createElement('ul')
-    newTask.textContent = userInputString
+    newTask.textContent = userInputString 
+
+// adding close button next to task item
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode(" \u00D7");
+    span.className = "close";
+    span.appendChild(txt); 
+
+    newTask.appendChild(span) 
 
     taskContainer.appendChild(newTask)
-  })
-})
+  })  
+
+// close button deletes task
+  // const close = document.getElementsByClassName("close");
+  // var j; 
+
+  // for (j = 0; j < close.length; j++) {
+  //   close[j].onclick = function() {
+  //     const div = this.parentElement;
+  //     div.style.display = "none";
+  //   }
+  // }
+
+
+}) 
+
+
+
+
+
+
+
